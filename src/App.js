c4ea716fbe88fe59b61ada3ba17a1bc7a45e735a
shreyash12s <88046814+shreyash12s@users.noreply.github.com>
2023-10-14 16:27:16 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div style={{
+      display:"flex",
+      height:"100vh",
+      justifyContent:"center",
+      alignItems:"center",
+      fontFamily:"cursive",
+      fontSize:44
+    }}>zoesync</div>
+  )
 }
-
-export default App;
